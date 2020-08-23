3ef71ba5e5db9c050f7301c2a7d220acb8c1898f
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2020-08-23 11:33:23 -0500

    Resolve problems with npm

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Register() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <p>
-        Amiguito mira estos recursos para manipular el estado en React sin
-        REDUX:
-      </p>
-      <ul>
-        <li>
-          <a href="https://medium.com/@leomedina.dev/react-administrar-el-estado-de-tu-app-bb560623fc55">
-            Article en español
-          </a>
-        </li>
-        <li>
-          <a href="https://kentcdodds.com/blog/application-state-management-with-react">
-            Article gringo muy bueno Primera parte
-          </a>
-        </li>
-        <li>
-          <a href="https://kentcdodds.com/blog/how-to-use-react-context-effectively">
-            Article gringo muy bueno Segunda parte
-          </a>
-        </li>
-      </ul>
-    </div>
+  <>
+  <p>Register</p>
+  </>
   );
 }
